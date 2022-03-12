aa5e778d5468f58544f48b21715f654a662ab312
umaaamm <umam.tekno@gmail.com>
2022-03-12 22:34:23 +0700

    [master][login-home] : login dan home

--- src/Action/ActionLogin.js
+++ src/Action/ActionLogin.js
@@ -3,8 +3,11 @@ import axios from "axios"
 export const actionLogin = async (param) => {
     try {
         const data = await axios.post('https://fe.runner.api.devcode.biofarma.co.id/auth/login', param);
+        if (data?.status == 200) {
+          return data
+        }
         return data;
     } catch (err) {
-        return err;
+        return err?.response;
     }
 }
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,6 +1,5 @@
 import { Form, Button } from 'react-bootstrap';
 import './Login.css';
-import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { actionLogin } from '../Action/ActionLogin'
 import { useDispatch, useSelector } from 'react-redux';
@@ -8,8 +7,11 @@ import LoginAction from '../Redux/Reducer/LoginAction';
 import { useNavigate } from 'react-router-dom';
 import Loading from '../Component/Loading';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+import * as React from 'react';
 
-export default function Login() {
+const Login = () => {
     const dispatch = useDispatch();
     const [email, setEmail] = useState('');
     const [loading, setLoading] = useState(false);
@@ -17,26 +19,96 @@ export default function Login() {
     const { token } = useSelector((state) => state.Login);
     const navigate = useNavigate();
     const [buttonDisable, setButtonDisable] = useState(false);
+    const [error, setError] = useState('');
+    const [open, setOpen] = useState(false);
+    const [severity, setSeverity] = useState('');
+    const [duration, setDuration] = useState(6000)
+    const [countEmail, setCountEmail] = useState(0)
+    const [countPassword, setCounPassword] = useState(0)
+    const [countLogin, setCounLogin] = useState(0)
+    const [countLoginLock, setCounLoginLock] = useState(0)
+
+
+
+    let regEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
 
     const handleLogin = async () => {
         try {
+
+            if (countPassword.length > 3 || countEmail.length > 3) {
+                setError('Terlalu banyak percobaan, pastikan data Email dan Password anda benar.');
+                setSeverity('error');
+                setOpen(true);
+                return;
+            }
+
+            if (!regEmail.test(email)) {
+                setError('Format Email tidak sesuai');
+                setSeverity('error');
+                setOpen(true);
+
+                return;
+            }
+
+            if (password.length < 6) {
+                setError('Password Minimal 6 Karakter');
+                setSeverity('error');
+                setDuration(6000);
+                setOpen(true);
+
+                return;
+            }
+
             setLoading(true)
             const param = {
                 username: email,
                 password: password
             }
             let data = await actionLogin(param);
-            if (data?.status == 200) {
-                dispatch(LoginAction.SET_TOKEN(data?.data?.data?.token));
+
+            console.log('qkqkqkqkq', data);
+
+            if (!data?.data?.success) {
+                if (data?.data?.status === 400) {
+                    setCountEmail(countEmail + 1)
+                    setCounPassword(countPassword + 1);
+                }
+                if (data?.data?.status === 403) {
+                    setButtonDisable(true);
+                    setTimeout(() => {
+                        setButtonDisable(false);
+                    }, 1000)
+                }
+                if (data?.data?.status === 401) {
+                    setCounLogin(countLogin + 1);
+                }
+
                 setLoading(false);
-                navigate("/Home")
+                setError(data?.data?.message);
+                setSeverity('error');
+                setOpen(true);
+                return
             }
+
+            dispatch(LoginAction.SET_TOKEN(data?.data?.data?.token))
             setLoading(false);
+            navigate('/Home')
+            return;
         } catch (err) {
             setLoading(false);
-            console.log('errorrr ', err);
         }
     }
+
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
+    };
 
     useEffect(() => {
         if (password.length > 0 && email.length > 0) {
@@ -44,50 +116,45 @@ export default function Login() {
         }
     }, [password, email]);
 
+    useEffect(() => {
+        console.log('kakksskkss ', countLoginLock);
+
+    }, [countLoginLock]);
+
     return (
-        <div className="Login">
-            <header className="Login-header">
-                <img src={require('../asset/images/header-logo.png')} className="Login-logo" />
+        <div className="login">
+            <header className="login-header">
+                <img src={require("../asset/images/header-logo.png")} className="login-logo" data-cy="header-logo" />
             </header>
-            <div className='Login-container'>
-                <div className='Login-form'>
+            <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity={severity} sx={{ width: '100%' }} data-cy="form-alert-container">
+                    {error}
+                </Alert>
+            </Snackbar>
+            <div className="login-container">
+                <div className="login-form">
+                    <label className="login-label" data-cy="form-text-title">Login</label>
                     {loading && <Loading />}
-                   <label className='Login-label-login'>Login</label>
-                      <label className='label-text-user' data-cy='form-text-email'>
-                        Email</label>
-                    <input type="email" data-cy='form-input-email' className='form-text-input-user' value={email} onInput={e => setEmail(e.target.value)} />
-                    <label className='label-text-password'>
-                        Password</label>
-                    <input type="password" className='form-text-input-password' value={password} onInput={e => setPassword(e.target.value)} />
 
-                    <button type="submit" disabled={!buttonDisable} className='form-button' data-cy='form-button-login' onClick={handleLogin}>Login</button>
+                    <label className="label-input label-input-text" data-cy="form-text-email">
+                        Email
+                    </label>
+                    <input type="email" data-cy="form-input-email" className="form-input form-input-text" value={email} onInput={(e) => setEmail(e.target.value)} />
+                    <label className="label-input label-input-password" data-cy="form-text-password">Password</label>
+                    <input type="password" className="form-input form-input-password" value={password} onInput={(e) => setPassword(e.target.value)} data-cy="form-input-password" />
 
-                    
-                    <label className='Login-lewati' data-cy='form-button-skip'>Lewati Login</label>
+                    <button type="submit" disabled={!buttonDisable} className="form-button-login" data-cy="form-button-login" onClick={handleLogin}>
+                        Login
+                    </button>
 
-                    {/* <Form>
-                        <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                        <Button variant="primary" type="submit">
-                            Submit
-                        </Button>
-                    </Form> */}
+                    <label className="login-lewati" data-cy="form-button-skip" onClick={() => { navigate('/Home') }}>
+                        Lewati Login
+                    </label>
                 </div>
-                <img src={require('../asset/images/content-logo.png')} className="Login-img-content" />
+                <img src={require("../asset/images/content-logo.png")} className="login-img-content" data-cy="content-logo" />
             </div>
-
         </div>
     );
-
-
 }
+
+export default Login;
